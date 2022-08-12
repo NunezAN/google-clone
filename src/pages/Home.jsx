3abd51aa300e04ffcbdaf6a3ab9a7292aa37a4cd
NunezAN <alex101nunez@gmail.com>
2022-08-11 20:19:25 -0500

    finished header element

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import AppsIcon from '@mui/icons-material/Apps';
+import { Avatar } from '@mui/material';
 
 const Home = () => {
   return (
     <div className="home">
-      <h1>This is my homepage</h1>
       <div className="home__header">
         <div className="home__header--left">
           <Link to="/about">About</Link>
@@ -13,7 +14,9 @@ const Home = () => {
         </div>
         <div className="home__header--right">
           <Link to="/gmail">Gmail</Link>
-          <Link to="/images">Images</Link>
+          <Link to="/Images">Images</Link>
+          <AppsIcon className="icon icon-apps"/>
+          <Avatar sx={{ bgcolor: "#202124" }}className="icon icon-avatar"></Avatar>
           {/* Icon */}
           {/* Avatar */}
         </div>
